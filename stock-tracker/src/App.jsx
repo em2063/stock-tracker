267fa427a2ebeba4a7b5cfa0271209311ca69895
em2063 <ethanmcgrogan1@gmail.com>
2024-01-21 19:20:41 +0000

    adds navbar and new styles

--- stock-tracker/src/App.jsx
+++ stock-tracker/src/App.jsx
@@ -1,10 +1,14 @@
 import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import StockRow from "./components/test.jsx";
+import "./../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import NavBar from "./components/nav.jsx";
+import StockRow from "./components/stockCard.jsx";
+import "./index.css";
 
 function App() {
   return (
     <>
       <div className="App">
+        <NavBar />
         <div className="container">
           <table className="table mt-5">
             <thead>
