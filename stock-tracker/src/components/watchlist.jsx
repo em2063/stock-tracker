4fcd95d389cd4e91d6b41bd4d96b785d5e7b2814
em2063 <ethanmcgrogan1@gmail.com>
2024-01-22 20:50:06 +0000

    eded

--- stock-tracker/src/components/watchlist.jsx
+++ stock-tracker/src/components/watchlist.jsx
@@ -9,16 +9,19 @@ function WatchList() {
             Keep track of what's <span>popular</span>
           </h1>
         </div>
-        <div className="watchlist-content">
-          <div className="form-container">
-            <div class="form-floating mb-3">
-              <input
-                class="form-control"
-                id="floatingInput"
-                placeholder="aapl"
-              />
-              <label for="floatingInput">Search for a stock</label>
+        <div className="watchlist-outer-container">
+          <div className="watchlist-content">
+            <div className="form-container">
+              <div className="form-floating mb-3">
+                <input
+                  className="form-control"
+                  id="floatingInput"
+                  placeholder="aapl"
+                />
+                <label for="floatingInput">Search for a stock</label>
+              </div>
             </div>
+            <div className="list-grid"></div>
           </div>
         </div>
       </div>
