4d64aaaa8bc433014df9972efdd2a88bc1e8f6ac
em2063 <ethanmcgrogan1@gmail.com>
2024-01-22 19:08:13 +0000

    Adds search ba =r to watchlist

--- stock-tracker/src/components/watchlist.jsx
+++ stock-tracker/src/components/watchlist.jsx
@@ -2,9 +2,16 @@ import React from "react";
 
 function WatchList() {
   return (
-    <div className="watchlist">
-      <p>hello</p>
-    </div>
+    <>
+      <div className="watchlist">
+        <div className="form-container">
+          <div class="form-floating mb-3">
+            <input class="form-control" id="floatingInput" placeholder="aapl" />
+            <label for="floatingInput">Search for a stock</label>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
