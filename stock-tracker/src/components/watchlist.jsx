e7a1cb0f30514cec51340a35c5e4d9cfe02ade4d
em2063 <ethanmcgrogan1@gmail.com>
2024-01-22 19:27:04 +0000

    adds title to watchlist page

--- stock-tracker/src/components/watchlist.jsx
+++ stock-tracker/src/components/watchlist.jsx
@@ -4,10 +4,21 @@ function WatchList() {
   return (
     <>
       <div className="watchlist">
-        <div className="form-container">
-          <div class="form-floating mb-3">
-            <input class="form-control" id="floatingInput" placeholder="aapl" />
-            <label for="floatingInput">Search for a stock</label>
+        <div className="watchlist-text m-3">
+          <h1>
+            Keep track of what's <span>popular</span>
+          </h1>
+        </div>
+        <div className="watchlist-content">
+          <div className="form-container">
+            <div class="form-floating mb-3">
+              <input
+                class="form-control"
+                id="floatingInput"
+                placeholder="aapl"
+              />
+              <label for="floatingInput">Search for a stock</label>
+            </div>
           </div>
         </div>
       </div>
